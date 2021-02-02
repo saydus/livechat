0b200262cd420a10a77272ffdc4cd0e61bc17049
Hani Mohammed <hani.m17@iiits.in>
2021-02-02 13:01:10 +0530

    Add Slate.js

--- src/components/Example/Example.jsx
+++ src/components/Example/Example.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+// Import the Slate editor factory.
+import { createEditor } from 'slate';
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react';
 import styled from 'styled-components';
 import Box from 'components/Box';
 import Logo from 'assets/icons/logo.svg';
@@ -18,14 +22,34 @@ export default () => {
     // use `url` here inside the callback because it's asynchronous!
   });
 
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text contained in Slate.js.\n Click to edit!' }],
+    },
+  ]);
   return (
     <Example>
-      <div style={{ width: 300 }}>
+      <div style={{ width: 400 }}>
         <Box display="flex" alignItems="center">
           <img src={PickleRick} width={50} height={50} />
           <div>{greeting}</div>
         </Box>
         <div style={{ fontSize: 11 }}>Current tab: {URL}</div>
+        <div
+          style={{
+            background: '#EEE',
+            padding: 16,
+            borderRadius: 20,
+            marginTop: 30,
+            color: '#333',
+          }}
+        >
+          <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+            <Editable />
+          </Slate>
+        </div>
       </div>
     </Example>
   );
